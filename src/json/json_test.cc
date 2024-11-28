2b435efa0cfdc2b367cb18dae7b1eae6f44c81bb
Daniel Connelly <dhconnelly@gmail.com>
2024-11-28 13:54:12 +0100

    json: clients only create ptrs; fix request parse test

--- src/json/json_test.cc
+++ src/json/json_test.cc
@@ -6,31 +6,32 @@ namespace gabby {
 namespace json {
 
 TEST(JSON, ParseNumber) {
-    EXPECT_EQ(NumberValue(0), *Value::Parse("0"));
-    EXPECT_EQ(NumberValue(17), *Value::Parse("17"));
-    EXPECT_EQ(NumberValue(-32.4), *Value::Parse("-32.4"));
+    EXPECT_EQ(*Value::Number(0), *Value::Parse("0"));
+    EXPECT_EQ(*Value::Number(17), *Value::Parse("17"));
+    EXPECT_EQ(*Value::Number(-32.4), *Value::Parse("-32.4"));
 }
 
 TEST(JSON, ParseBoolean) {
-    EXPECT_EQ(BooleanValue(true), *Value::Parse("17"));
-    EXPECT_EQ(BooleanValue(false), *Value::Parse("0"));
+    EXPECT_EQ(*Value::Boolean(true), *Value::Parse("17"));
+    EXPECT_EQ(*Value::Boolean(false), *Value::Parse("0"));
 }
 
 TEST(JSON, ParseString) {
-    EXPECT_EQ(StringValue(""), *Value::Parse("\"\""));
-    EXPECT_EQ(StringValue("foo bar"), *Value::Parse("\"foo bar\""));
+    EXPECT_EQ(*Value::String(""), *Value::Parse("\"\""));
+    EXPECT_EQ(*Value::String("foo bar"), *Value::Parse("\"foo bar\""));
 }
 
 TEST(JSON, ParseArray) {
-    EXPECT_EQ(ArrayValue({}), *Value::Parse("[]"));
-    EXPECT_EQ(ArrayValue({Value::Boolean(true), Value::String("abc")}),
+    EXPECT_EQ(*Value::Array({}), *Value::Parse("[]"));
+    EXPECT_EQ(*Value::Array({Value::Boolean(true), Value::String("abc")}),
               *Value::Parse("[true, \"abc\"]"));
-    EXPECT_EQ(ArrayValue({}), *Value::Parse("[]"));
+    EXPECT_EQ(*Value::Array({Value::Array({}), Value::Array({})}),
+              *Value::Parse("[[],[]]"));
 }
 
 TEST(JSON, ParseObject) {
-    EXPECT_EQ(ObjectValue({}), *Value::Parse("{}"));
-    EXPECT_EQ(ObjectValue({
+    EXPECT_EQ(*Value::Object({}), *Value::Parse("{}"));
+    EXPECT_EQ(*Value::Object({
                   {std::string("a"), Value::String("b")},
                   {std::string("c"), Value::Number(1)},
               }),
@@ -38,7 +39,22 @@ TEST(JSON, ParseObject) {
 }
 
 TEST(JSON, ParseCompletionRequest) {
-    std::string json = R"(
+    EXPECT_EQ(
+        *Value::Object({
+            {"model", Value::String("gabby-1")},
+            {"messages",
+             Value::Array({Value::Object({
+                               {"role", Value::String("system")},
+                               {"content",
+                                Value::String("You are a helpful assistant.")},
+                           }),
+                           Value::Object({
+                               {"role", Value::String("user")},
+                               {"content", Value::String("Hello!")},
+                           })})},
+            {"stream", Value::Boolean(true)},
+        }),
+        *Value::Parse(R"(
         {
             "model": "gabby-1",
             "messages": [
@@ -53,8 +69,7 @@ TEST(JSON, ParseCompletionRequest) {
             ],
             "stream": true
         }
-    )";
-    EXPECT_EQ(1, 1);
+    )"));
 }
 
 }  // namespace json
